27698ba4cfae90bcbf1e993fd656099b9c2a9291
Shawn Arlantico <arlantico.shawn@gmail.com>
2024-06-21 21:52:53 -0700

    fixed duplication of user count issue

--- MiniTwitterApp/src/main/models/AnalyticsVisitor.java
+++ MiniTwitterApp/src/main/models/AnalyticsVisitor.java
@@ -1,25 +1,42 @@
 package main.models;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class AnalyticsVisitor implements Visitor {
    private int userCount = 0;
    private int groupCount = 0;
    private int tweetCount = 0;
    private int positiveTweetCount = 0;
+   private Set<String> uniqueUsers = new HashSet<>();
+   private Set<String> uniqueGroups = new HashSet<>();
+
+   private static final Set<String> positiveWords = Set.of("good", "great", "excellent", "fantastic", "awesome");
 
    @Override
    public void visit(User user) {
-      userCount++;  // Increment count when visiting a user
-      tweetCount += user.getNewsFeed().size();
-      positiveTweetCount += (int) user.getNewsFeed().stream().filter(tweet -> isPositiveTweet(tweet)).count();
+      if (uniqueUsers.add(user.getId())) { // Ensure the user is counted only once
+         userCount++;
+         tweetCount += user.getNewsFeed().size();
+         for (String tweet : user.getNewsFeed()) {
+               for (String word : positiveWords) {
+                  if (tweet.toLowerCase().contains(word)) {
+                     positiveTweetCount++;
+                     break;
+                  }
+               }
+         }
+      }
    }
 
    @Override
    public void visit(Group group) {
-      groupCount++;  // Increment count when visiting a group
-      for (Component component : group.getChildren()) {
-         component.accept(this);  // Recursively visit all children
+      if (uniqueGroups.add(group.getId())) { // Ensure the group is counted only once
+         groupCount++;
+         for (Component component : group.getChildren()) {
+               component.accept(this);
+         }
       }
    }
 
@@ -36,11 +53,6 @@ public class AnalyticsVisitor implements Visitor {
    }
 
    public double getPositiveTweetPercentage() {
-      return tweetCount > 0 ? 100.0 * positiveTweetCount / tweetCount : 0;
-   }
-
-   private boolean isPositiveTweet(String tweet) {
-      String[] positiveWords = {"good", "great", "excellent", "nice", "awesome", "fantastic"};
-      return Arrays.stream(positiveWords).anyMatch(tweet.toLowerCase()::contains);
+      return tweetCount == 0 ? 0 : (double) positiveTweetCount / tweetCount * 100;
    }
 }
